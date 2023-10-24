41e1c57f1dde3613423bde3c2a032df4252d0fc9
null <null>
2023-10-24 20:15:55 +0000

    all good

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express"
 
+/*
 const PORT = 3000
 const app = express()
 
@@ -10,3 +11,22 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}..`)
 })
+*/
+
+'use strict';
+ 
+//const express = require('express');
+ 
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+ 
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+ 
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
